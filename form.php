c8cc489b583265bda453c6b96a2ef12873f1eecb
Alyssa Smith <asmithh@mit.edu>
2016-01-31 15:21:54 -0500

    first goddamn commit

---
+++ form.php
@@ -0,0 +1,11 @@
+<html>where am i</html>
+<?php 
+echo "<html> Is there anyone out there? </html>";
+$provider = $_POST['provider'];
+$price = $_POST['price'];
+$upload = $_POST['upload'];
+$download = $_POST'download';
+$datacap = $_POST 'datacap';
+$formcontent="From: $name \n Message: $message";
+echo "<html> Thank You! </html>";
+?>
